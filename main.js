37ff87d855c9d3db253211c2b24c5db9e91982b4
alexus21 <US21003@ues.edu.sv>
2023-08-22 20:39:33 -0600

    Estructura base del prototipo (no concluida)

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 function Pokemon(){
 
-    const showSpecies = function (){
-        return "Pokemon gei";
+    const showPokemonSpecies = function (){
+        return "Especie: ";
+    }
+
+    const showPokemonHeight = function (){
+        return "Altura: ";
+    }
+
+    const showPokemonWeight = function (){
+        return "Peso: ";
+    }
+
+    const showPokemonType = function (){
+        return "Tipo: ";
+    }
+
+    const showPokemonStatistics = function (){
+        return "Habilidades: ";
     }
 
 }
